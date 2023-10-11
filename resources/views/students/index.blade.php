9e8df5aae2fd23155b76394955dce56b223ff8f4
markvillanueva08 <monkeytheluffy08@gmail.com>
2023-10-11 11:19:24 +0800

    pahabol lesson 6

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,57 +1,6 @@
 
 @include('partials.header')
-<nav x-data="{ open: false }" class="bg-gray-800 text-white fixed w-full z-20 top-0 left-0 border-gray-400-px-2 sm:px-4 py-2.5 ">
-    <div  class="container flex flex-wrap justify-between item-center">
-        <a href="/">
-            <span class="self-center text-xl font-semibold whitespace-nowrap">
-                Student System
-            </span>
-        </a>
-        <button @click="open = !open" data-collapse-toggle="navbar-main" class="md:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
-        </button>
-        
-        <div x-show="open" class=" w-full md:block md:w-auto" id="navbar-main">
-            <ul class="flex flex-col md:flex-row px-4 ">
-                <li>
-                    <a href="#" class="block py-2 pr-4 pl-3 ">
-                        Sign in
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="block py-2 pr-4 pl-3 ">
-                        Sign Up
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="block py-2 pr-4 pl-3 ">
-                        Log Out
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div hidden class=" w-full md:block md:w-auto" id="navbar-main">
-            <ul class="flex flex-col md:flex-row px-4 ">
-                <li>
-                    <a href="#" class="block py-2 pr-4 pl-3 ">
-                        Sign in
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="block py-2 pr-4 pl-3 ">
-                        Sign Up
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="block py-2 pr-4 pl-3 ">
-                        Log Out
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-  
-</nav>
+<x-nav/>
 
 <header class="max-w-lg mx-auto mt-5">
     <a href="">
