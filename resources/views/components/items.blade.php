8dcdfcf009543fa6668058e31766f1cc9a2847fd
markvillanueva08 <monkeytheluffy08@gmail.com>
2023-11-11 17:12:39 +0800

    crud  part 8

--- resources/views/components/items.blade.php
+++ resources/views/components/items.blade.php
@@ -1,12 +1,8 @@
 <ul class="flex flex-col md:flex-row px-4 ">
+    @auth
     <li>
-        <a href="/login" class="block py-2 pr-4 pl-3 ">
-            Sign in
-        </a>
-    </li>
-    <li>
-        <a href="/register" class="block py-2 pr-4 pl-3 ">
-            Sign Up
+        <a href="/add/student" class="block py-2 pr-4 pl-3 ">
+            Add New
         </a>
     </li>
     <li>
@@ -17,4 +13,17 @@
         </button>
     </form>
     </li>
+   @else
+    <li>
+        <a href="/login" class="block py-2 pr-4 pl-3 ">
+            Sign in
+        </a>
+    </li>
+    <li>
+        <a href="/register" class="block py-2 pr-4 pl-3 ">
+            Sign Up
+        </a>
+    </li>
+    @endauth
+  
 </ul>
